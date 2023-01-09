9d461cd6cdf1ccb0e4439fab829f6b71438eca2b
Jason Shi <jasonxshi2002@gmail.com>
2023-01-09 15:11:21 -0600

    many links

--- generate.py
+++ generate.py
@@ -2,7 +2,20 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1.0
+width = 2.0
+height = 3.0
+
+x = 0.0
+y = 0.0
+z = 1.5
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+#pyrosim.Send_Cube(name="Box2", pos=[0,1,1.5] , size=[length,width,height])
+
+# for j in range(5):
+#     for k in range(5):
+#         for i in range(10):
+#             pyrosim.Send_Cube(name=str(i), pos=[j,k,0.5+i] , size=[0.9**i*length,0.9**i*width,0.9**i*height])
 
 pyrosim.End()
 
