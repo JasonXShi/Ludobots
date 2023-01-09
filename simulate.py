e450fd5b0acc87855dc41096007358d63b99f541
Jason Shi <jasonxshi2002@gmail.com>
2023-01-09 13:59:35 -0600

    added steps and timer

--- simulate.py
+++ simulate.py
@@ -1,5 +1,11 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+p.stepSimulation()
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1./60.)
+    print(i)
 
 p.disconnect()
